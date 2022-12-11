cff513f0212caae5c2e1aeeaa9616265ffcf7c33
Maciej Sugajski <maciejsugajski@macbook-pro-suga.home>
2022-12-11 10:00:38 +0100

    Added loading.

--- App.js
+++ App.js
@@ -9,19 +9,21 @@ const App = () => {
   const {requestPermissions} = useBLE();
 
   const openModal = async (permissionType) => {
+    setIsLoading(true);
     console.log('pressed');
 
     const permission = await requestPermissions(permissionType);
+    setIsLoading(false);
 
       alert('granted? ' + permission);
   };
   return (
     <View style={styles.container}>
       <Text>BT TEST</Text>
-      {isLoading  && <ActivityIndicator />}
       <View style={styles.buttonsContainer}>
         <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)} title="Learn More" />
         <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT)} title="Bluetooth connect permission" />
+        {isLoading  && <ActivityIndicator />}
         <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN)} title="Bluetooth scan permission" />
         <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADVERTISE)} title="Bluetooth advertise permission" />
       </View>
