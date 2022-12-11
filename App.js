0d54b106f3631e3d24a2cec441a3467fc4cc930f
Maciej Sugajski <maciejsugajski@macbook-pro-suga.home>
2022-12-11 09:56:05 +0100

    Fixed bluetooth permissions.

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button, ActivityIndicator, PermissionsAndroid} from 'react-native';
 import useBLE from './useBLE';
 import {BleManager} from 'react-native-ble-plx';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
   //const bleManager = new BleManager();
   const {requestPermissions} = useBLE();
 
-  const openModal = async () => {
+  const openModal = async (permissionType) => {
     console.log('pressed');
 
-    requestPermissions(isGranted => {
-      alert('granted? ' + isGranted);
-    });
+    const permission = await requestPermissions(permissionType);
+
+      alert('granted? ' + permission);
   };
   return (
     <View style={styles.container}>
       <Text>BT TEST</Text>
-      <Button onPress={openModal} title="Learn More" />
+      {isLoading  && <ActivityIndicator />}
+      <View style={styles.buttonsContainer}>
+        <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)} title="Learn More" />
+        <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT)} title="Bluetooth connect permission" />
+        <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN)} title="Bluetooth scan permission" />
+        <Button onPress={() => openModal(PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADVERTISE)} title="Bluetooth advertise permission" />
+      </View>
     </View>
   );
 };
@@ -28,5 +35,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
+  },
+  buttonsContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
   },
 });
